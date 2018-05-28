27eafcab3c3bf1aa035d8c00e4aeda3c41cd7345
Your Namehglottman <hglottman@gmail.com>
2018-05-28 18:07:22 +0300

    blat

--- src/app/add-dog/add-dog.component.ts
+++ src/app/add-dog/add-dog.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DogsService } from '../dogs.service';
+import { Dog } from '../dog';
+
 
 @Component({
   selector: 'app-add-dog',
   templateUrl: './add-dog.component.html',
   styleUrls: ['./add-dog.component.scss']
 })
+
 export class AddDogComponent implements OnInit {
 
   title: string = 'AngularDogs';
-  dogName: string = 'Rex';
-  dogWeight: number;
-  ownerName: string = 'John Doe';
+  dog: Dog = new Dog();
   imageUrl: string;
 
-  constructor() {
+
+  constructor(public dogsService: DogsService) {
     var currentDate = new Date();
     if (currentDate.getHours() > 21 && currentDate.getHours() <= 6) {
       this.imageUrl = 'https://besthqwallpapers.com/Uploads/22-1-2018/37943/thumb2-4k-moon-dog-night-digital-art.jpg';
@@ -21,6 +24,10 @@ export class AddDogComponent implements OnInit {
     else {
       this.imageUrl = 'http://imgsrv.wkdzradio.com/image/wkdz4/UserFiles/Image/Dog%20Day1.jpg';
     }
+  }
+
+  submitDog() {
+      this.dogsService.addDog(this.dog);
   }
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { AddDogComponent } from './add-dog/add-dog.component';
 import { AddOwnerComponent } from './add-owner/add-owner.component';
 import { DogsComponent } from './dogs/dogs.component';
+import { DogsService } from './dogs.service';
+
 
 
 @NgModule({
@@ -32,7 +32,7 @@ import { DogsComponent } from './dogs/dogs.component';
     MatButtonModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DogsService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
